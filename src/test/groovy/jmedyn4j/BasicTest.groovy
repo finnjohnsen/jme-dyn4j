d6fe4c1583e46da124fe4228f5bf6ce6cf58b978
finn <finn@twister.lan>
2015-11-08 20:56:56 +0100

    removing unceesary stuff

--- src/test/groovy/jmedyn4j/BasicTest.groovy
+++ src/test/groovy/jmedyn4j/BasicTest.groovy
@@ -1,7 +1,5 @@
 package jmedyn4j
 
-import javax.vecmath.Vector2f;
-
 import org.dyn4j.dynamics.Body
 import org.dyn4j.dynamics.BodyFixture
 import org.dyn4j.dynamics.World
@@ -13,6 +11,7 @@ import org.dyn4j.geometry.Transform
 import com.jme3.app.SimpleApplication
 import com.jme3.material.Material
 import com.jme3.math.ColorRGBA
+import com.jme3.math.Vector2f
 import com.jme3.math.Vector3f
 import com.jme3.scene.Geometry
 import com.jme3.scene.shape.Box
